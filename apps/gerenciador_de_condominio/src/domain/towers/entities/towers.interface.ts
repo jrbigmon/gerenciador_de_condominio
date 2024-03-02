2bd2c2328f28e890357e6c175c56e9c3be6ff140
jrbigmon <juniorterriaga0@gmail.com>
2024-03-02 10:57:15 -0300

    feat: optional properties id and apartments

--- apps/gerenciador_de_condominio/src/domain/towers/entities/towers.interface.ts
+++ apps/gerenciador_de_condominio/src/domain/towers/entities/towers.interface.ts
@@ -1,6 +1,7 @@
 import { ApartmentInterface } from '../../apartments/entities/apartments.interface';
 
 export interface TowerInterface {
+  id?: string;
   name: string;
-  apartments: ApartmentInterface[];
+  apartments?: ApartmentInterface[];
 }
