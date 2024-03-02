d744b378c11cb809bbd8b17b73fba9bd367fdcc5
jrbigmon <juniorterriaga0@gmail.com>
2024-03-02 11:28:53 -0300

    feat: get list and inject in memory

--- apps/gerenciador_de_condominio/src/domain/users/users.controller.ts
+++ apps/gerenciador_de_condominio/src/domain/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UserService } from './users.service';
 import { UserCreateDto } from './dto/user-create.dto';
 import { UserEntity } from './entities/users.entity';
@@ -11,6 +11,16 @@ export class UserController {
   public async create(@Body() body: UserCreateDto): Promise<UserEntity> {
     try {
       return await this.userService.create(body);
+    } catch (error) {
+      console.log(error);
+      throw error;
+    }
+  }
+
+  @Get()
+  public async list(): Promise<UserEntity[]> {
+    try {
+      return await this.userService.list();
     } catch (error) {
       console.log(error);
       throw error;
--- apps/gerenciador_de_condominio/src/domain/users/users.service.ts
+++ apps/gerenciador_de_condominio/src/domain/users/users.service.ts
@@ -1,12 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { UserEntity } from './entities/users.entity';
 import { UserCreateDto } from './dto/user-create.dto';
+import { UserRepositoryInterface } from './entities/users.repository.interface';
+import { UserInMemoryRepository } from './entities/users.in.memory.repository';
 
 @Injectable()
 export class UserService {
+  private readonly userRepository: UserRepositoryInterface;
+
+  constructor() {
+    this.userRepository = new UserInMemoryRepository();
+  }
+
   public async create(record: UserCreateDto): Promise<UserEntity> {
     const userCreated = UserEntity.create(record);
 
+    this.userRepository.create(userCreated);
+
     return userCreated;
+  }
+
+  public async list(): Promise<UserEntity[]> {
+    return this.userRepository.findAll();
   }
 }
